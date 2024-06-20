8c8c3634cb2c383142c93f686e4efad8228cddcd
Rizki Taufik Anindhita <rizki.anindhita@binus.ac.id>
2024-06-20 09:22:46 +0700

    update

--- app/number/page.tsx
+++ app/number/page.tsx
@@ -26,7 +26,7 @@ const Number = () => {
   return (
   <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff66_1px,#1E293B_1px)] bg-[size:20px_20px]">
       <div className="flex justify-center items-center h-screen">
-        <div className="w-64 h-fit sm:w-3/12 sm:h-4/6 2xl:w-2/12 2xl:h-2/5 rounded-xl bg-slate-600 ">
+        <div className="w-[350px] h-fit md:w-[600px] xl:w-[400px] rounded-xl bg-slate-600 ">
           <div onClick={()=>router.push("/")} className="ml-5 mt-3 text-white">
           <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -34,7 +34,7 @@ const Number = () => {
 </button>
           </div>
           <div className="flex justify-center items-center p-5 text-slate-300 text-md md:text-xl">ANGKA</div>
-          <div className="flex justify-center items-center pb-10 font-bold text-8xl md:text-9xl text-slate-50">{number}</div>
+          <div className="flex justify-center items-center pb-10 font-bold text-[300px] md:text-[400px] xl:text-[200px] text-slate-50">{number}</div>
           <div className="flex justify-center items-center">
             <button className="text-slate-300 text-sm md:text-base" onClick={reset}>reset</button>
           </div>
